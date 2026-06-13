ccd0972e4054bdd09110b182601da32b7cf75eaf
non <168718969+non123a@users.noreply.github.com>
2026-06-13 10:09:58 +0700

    changing setting.py sstatic url to django static

--- config/settings.py
+++ config/settings.py
@@ -132,7 +132,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-STATIC_URL = '/static/'
+STATIC_URL = '/django-static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
